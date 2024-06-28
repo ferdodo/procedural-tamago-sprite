a04368bbad3171893443f1c6bb0b6f1135f4b5e8
Thomas Riffard <ferdodo@hotmail.fr>
2024-06-28 11:57:05 +0200

    remove crumbs and refactor

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,38 +1,5 @@
-import "crumbs-design-system";
 import init, { generatetamago } from "../public/procedural_tamago_sprite";
-
-function generateImageFromHexString(buffer, width) {
-  if (buffer.length % 8 !== 0) {
-    throw new Error("Le buffer doit avoir une longueur multiple de 8.");
-  }
-
-  const height = Math.ceil(buffer.length / (width * 8));
-
-  const canvas = document.createElement("canvas");
-  canvas.width = width;
-  canvas.height = height;
-  const ctx = canvas.getContext("2d");
-  const imageData = ctx.createImageData(width, height);
-
-  for (let i = 0; i < buffer.length; i += 8) {
-    const r = parseInt(buffer.slice(i, i + 2), 16);
-    const g = parseInt(buffer.slice(i + 2, i + 4), 16);
-    const b = parseInt(buffer.slice(i + 4, i + 6), 16);
-    const a = parseInt(buffer.slice(i + 6, i + 8), 16);
-    const x = (i / 8) % width;
-    const y = Math.floor((i / 8) / width);
-    const index = (y * width + x) * 4;
-    imageData.data[index] = r;
-    imageData.data[index + 1] = g;
-    imageData.data[index + 2] = b;
-    imageData.data[index + 3] = 255;
-  }
-
-  ctx.putImageData(imageData, 0, 0);
-  const img = document.createElement("img");
-  img.src = canvas.toDataURL();
-  return img;
-}
+import { generateImageFromHexString } from "./generate-image-from-hexstring";
 
 init().then(function() {
 	window.addEventListener('wheel', (event) => {
